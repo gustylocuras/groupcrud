24550f2805b403893789d17c90b63d96d642d99c
Agustin Alvarez <agustinalvarez@agustins-mbp.home>
2020-06-16 07:52:12 -0700

    hello world

--- bike_server.js
+++ bike_server.js
@@ -2,5 +2,9 @@ const express = require('express')
 const app = express()
 
 app.listen(3000, (req, res) => {
+  console.log('listening...');
+})
+
+app.get('/', (req, res) => {
   res.send('hello world')
 })
