cefa2ddae6d647d7b4607b81922af2fa842b0561
MyMac <mymac@MyMacs-MBP.home>
2020-06-16 18:06:18 -0400

    app.js work

--- public/app.js
+++ public/app.js
@@ -1,1 +1,60 @@
-const app = angular.module("MyApp", [])
+const app = angular.module("MyApp", []);
+
+app.controller("BikeController", [
+  "$http",
+  function ($http) {
+    this.title = "";
+    this.image = "";
+    this.url = "";
+    this.bikes = [];
+
+    this.deleteBike = (bike) => {
+      $http({
+        method: "DELETE",
+        url: "/bikes/" + bike._id,
+      }).then(
+        (response) => {
+          this.getBikes();
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
+    };
+
+    this.createBike = () => {
+      $http({
+        method: "POST",
+        url: "/bikes",
+        data: {
+          title: this.title,
+          image: this.image,
+          url: this.url,
+        },
+      }).then(
+        (response) => {
+          this.bikes.unshift(response.data);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    };
+
+    this.getBikes = () => {
+      $http({
+        method: "GET",
+        url: "/bikes",
+      }).then(
+        (response) => {
+          console.log(response);
+          this.bikes = response.data;
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
+    };
+    this.getBikes();
+  },
+]); // close of controller
